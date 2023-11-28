b7b3a2fed2d31420f3ad78426622cea6e798589f
Raymond Doetjes <rdoetjes@phonax.com>
2023-11-28 17:40:33 +0100

    Introduced hashmap and colors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, collections::HashMap};
 
 use sysinfo::{System, SystemExt };
 
@@ -13,28 +13,40 @@ fn round(x: f64, decimals: u32) -> f64 {
     (x * y).round() / y
 }
 
-fn replace_data(logo: &str) -> String{
+fn replace_data(mut logo: String) -> String{
     let mut sys = System::new();
     sys.refresh_all();
     let na = "Can't obtain info";
-
-    let logo = logo.replace("<name>", &sys.name().expect(na));
-    let logo = logo.replace("<kernel_version>", &sys.kernel_version().expect(na));
-    let logo = logo.replace("<os_version>", &sys.os_version().expect(na));
-    let logo = logo.replace("<cpu_len>", &sys.cpus().len().to_string());
-
+    
     let mut m_gb = (sys.total_memory()/(1024*1024*1024)).to_string();
     if m_gb == "15" {  m_gb = "16".to_owned() }
-    let logo = logo.replace("<total_memory>", &m_gb);
 
-    let logo = logo.replace("<load.one>", &(round(sys.load_average().one, 2).to_string()));
-    let logo = logo.replace("<load.five>", &(round(sys.load_average().five, 2).to_string()));
-    let logo = logo.replace("<load.fifteen>", &(round(sys.load_average().fifteen,2).to_string()));
+    let replacement = HashMap::from([
+        ("<name>", sys.name().expect(na)),
+        ("<kernel_version>", sys.kernel_version().expect(na)),
+        ("<os_version>", sys.os_version().expect(na)),
+        ("<cpu_len>", sys.cpus().len().to_string()),
+        ("<total_memory>", m_gb),
+        ("<load.one>", (round(sys.load_average().one, 2).to_string())),
+        ("<load.five>", (round(sys.load_average().five, 2).to_string())),
+        ("<load.fifteen>", (round(sys.load_average().fifteen, 2).to_string())),
+        ("<r>", "\x1b[31m".to_string()),
+        ("<g>", "\x1b[32m".to_string()),
+        ("<y>", "\x1b[33m".to_string()),
+        ("<b>", "\x1b[34m".to_string()),
+        ("<m>", "\x1b[35m".to_string()),
+        ("<c>", "\x1b[36m".to_string()),
+        ("<w>", "\x1b[37m".to_string()),
+        ]);
+
+    for kv in replacement {
+       logo = logo.replace(kv.0, &kv.1).clone();
+    }
     logo.to_owned()
 }
 
 fn main() {
     let mut logo = read_logo(&(std::env::var("HOME").expect("CAN'T FIND HOME VARIABLE")+"/.config/rfetch/message.txt"));
-    logo = replace_data(&logo);
+    logo = replace_data(logo);
     print!("{}", logo);
 }
